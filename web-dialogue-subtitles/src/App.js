a6ee05bd098e0473b8379dcb3ceee7db210e65a3
Roger Wang <rogerwangcs@gmail.com>
2019-04-06 13:24:40 -0400

    clean

--- web-dialogue-subtitles/src/App.js
+++ web-dialogue-subtitles/src/App.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: {}
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello World!</h1>
       </div>
     );
+
   }
 }
 
