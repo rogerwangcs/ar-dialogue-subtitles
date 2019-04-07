3270b781ecf223828d2a984f5f6c9071841739d2
David Shen <me@pantherman594.com>
2019-04-07 06:06:14 -0400

    Import face-api into isTalking

--- web-dialogue-subtitles/src/isTalking.js
+++ web-dialogue-subtitles/src/isTalking.js
@@ -1,4 +1,6 @@
-const personTalking = (lipsArray) => {
+import * as faceapi from 'face-api.js';
+
+export const personTalking = (lipsArray) => {
     let x = 0;
     let y = 0;
     for (const lip of lipsArray){
@@ -10,7 +12,7 @@ const personTalking = (lipsArray) => {
 
     let dist = 0;
     for (const lip of lipsArray){
-        dist += euclideanDistance([averageX, averageY], [lip._x, lip._y]);
+        dist += faceapi.euclideanDistance([averageX, averageY], [lip._x, lip._y]);
     }
 
     return dist
